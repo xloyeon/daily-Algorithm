0e15c42e0d72ff70c63109d2dd2e29dc3b12d7db
syahn <496300@naver.com>
2022-01-27 23:24:16 +0900

    Add_newFile_n1427

---
+++ n1427.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String n = br.readLine();
+		int[] arr = new int[n.length()];
+		
+		for(int i = 0; i<n.length(); i++) {
+			int a = n.charAt(i) -'0';
+			arr[i] = a;
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i = arr.length-1; i>=0; i--) {
+			System.out.print(arr[i]);
+		}
+	}
+}
