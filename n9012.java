286150ff82efc110fdd36380df60f64630c660b2
syahn <496300@naver.com>
2022-01-29 21:31:32 +0900

    Add_newFile_n9012

---
+++ n9012.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i = 0 ; i < t ; i++) {
+			String s = br.readLine();
+			int countA = 0, countB = 0;
+			char[] arr = s.toCharArray();
+			
+			for(char c: arr) {
+				if(c== '(') {
+					countA += 1;
+				}else{
+					countB += 1;
+				}
+				
+				if(countA<countB)
+					break;
+			}
+			
+			if(countA == countB)
+				sb.append("YES\n");
+			else
+				sb.append("NO\n");
+			
+		}
+		System.out.println(sb);
+		}	
+}
