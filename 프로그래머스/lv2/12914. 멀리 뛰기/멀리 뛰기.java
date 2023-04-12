2c4750ff628991b7b7c0f959491b11fb36afccfb
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-04-12 16:26:41 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    
+    public long solution(int n) {
+        long[] result = new long[n];
+        
+        for(int i = 0; i<n; i++){
+            if(i==0) result[i] = 1;
+            else if(i==1) result[i] = 2;
+            else{
+                result[i] = (result[i-1] + result[i-2])%1234567;
+            }
+        }
+        return result[n-1];
+    }
+}
