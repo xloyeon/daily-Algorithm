1618a188a8072e9f6d5703004e82d5c5fdd7fbdc
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-05-05 23:58:40 +0900

    [level 2] Title: 타겟 넘버, Time: 6.71 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,31 +1,26 @@
 import java.util.*;
 
 class Solution {
-
+    
+    //BFS
     public int solution(int[] numbers, int target) {
         int count = 0;
         
-        Queue<int[]> q = new LinkedList<>();
-        
-        q.add(new int[]{0, numbers[0]});
-        q.add(new int[]{0, -numbers[0]});
+        //{numbers의 index, 현재 누적합}
+        Queue<int[]> q = new LinkedList<>(); 
+        q.add(new int[]{0, 0});
         
         while(!q.isEmpty()){
             int[] x = q.poll();
             
-            if(x[0] == numbers.length-1 && x[1] == target){
+            if(x[0]>=numbers.length && x[1] == target)
                 count++;
-                continue;
-            }
             
-            int idx = x[0] + 1;
-            
-            if(idx < numbers.length){
-                q.add(new int[]{idx, x[1]+numbers[idx]});
-                q.add(new int[]{idx, x[1]-numbers[idx]});
+            if(x[0]>=0 && x[0]<numbers.length){
+                q.add(new int[]{x[0]+1, x[1]+numbers[x[0]]});
+                q.add(new int[]{x[0]+1, x[1]-numbers[x[0]]});
             }
         }
-        
         return count;
     }
 }
