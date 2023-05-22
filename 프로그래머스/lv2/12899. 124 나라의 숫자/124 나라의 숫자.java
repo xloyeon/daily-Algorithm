eca34accc2ab27e85f0f1669cf37c5829a0cc7d1
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-05-22 19:32:57 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.06 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 class Solution {
     public String solution(int n) {
-        int temp = 3;
+        
+        int temp = 3;   //3은 미리 더해놓기
         int idx = 2;
         
         while(n>temp){
-            temp += Math.pow(3, idx);
+            temp += Math.pow(3, idx);   //3의 제곱수만큼 늘어남
             idx++;
         }
         
         //8일 때, idx = 3
-        //13일 떼, idx = 4 111
+        //13일 때, idx = 4 111
         StringBuilder sb = new StringBuilder();
         
         
-        
-        int start = ((int)(temp-Math.pow(3, idx-1))+1); 
-        int m = n-start;
+        int start = ((int)(temp-Math.pow(3, idx-1))+1); //현재수(temp)에서 전단계(떨어지는 수)
+        int m = n-start;    //떨어지지 않는 만큼(13일 때 0)
         int pow = idx-2;
 
         for(int i=0; i<idx-1; i++){
