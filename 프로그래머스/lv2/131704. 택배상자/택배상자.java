3996a673eaba18bab793bbb21046df1226d7b19c
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-05-22 19:36:36 +0900

    [level 2] Title: 택배상자, Time: 177.33 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/lv2/131704. 택배상자/택배상자.java
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.java
@@ -16,18 +16,19 @@ class Solution {
         }
         
         for(int i = 0; i<order.length;i++){
-            if(!q.isEmpty() && q.peek() == order[i]){
+            if(!q.isEmpty() && q.peek() == order[i]){   //컨테이너벨트의 다음 순서가 실어야 하는 상자와 같으면
                 q.poll();
                 count++;
                 continue;
-            }else if(!stack.isEmpty() && stack.peek() == order[i]){
+            }else if(!stack.isEmpty() && stack.peek() == order[i]){ //보조 컨테이너벨트의 상단이 실어야 하는 상자와 같으면
                 stack.pop();
                 count++;
                 continue;
-            }else if(q.isEmpty()){
+            }else if(q.isEmpty()){  //보조에서만 꺼낼 수 있는데 순서가 맞지 않으므로 꺼낼 수 없음
                 break;
             }
             
+            //컨테이너벨트에서 꺼내야 하는 상자를 꺼내기 위해 앞의 상자들 전부 보조로 옮김
             if(!q.isEmpty()){
                 stack.add(q.poll());
                 i--;
