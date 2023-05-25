b9b04db4b0391b47abe2743d5fad45cd35d69f96
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-05-26 01:01:41 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.06 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -2,22 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, long k) {
-        int[] ret = new int[n];
-		List<Integer> list = new ArrayList<>();
-        long num=1;
-        int idx=0;
-		for(int x=1 ; x<=n ; x++) {
-			list.add(x);
-			num *= x;
+        
+        long[] factorials = new long[n+1]; //factorials[n-1] = n-1!
+        
+		List<Integer> nums = new ArrayList<>(); //사람들 숫자 1~n
+        int[] result = new int[n];  //결과 배열
+        
+        factorials[0] = 1L;
+        
+		for(int i=1 ; i<=n ; i++) {
+            nums.add(i);
+			factorials[i] = factorials[i-1]*i;
 		}
+        
         k--;
-        while(idx<ret.length) {
-        	num /= (n--);
-        	int i = (int)(k/num);
-        	ret[idx++] = list.get(i);
-        	list.remove(i);
-        	k %= num;
+        
+        for(int i = n-1; i>=0; i--){
+        	int temp = (int)(k/factorials[i]);
+        	result[n-i-1] = nums.get(temp);
+        	nums.remove(temp);
+        	k %= factorials[i];
         }
-        return ret;
+        
+        return result;
     }
 }
