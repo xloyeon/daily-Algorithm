32b72b82dc722b52540d0d103291ceba3eaba75c
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-05-26 01:25:18 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.07 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -5,8 +5,9 @@ class Solution {
         //전체 박스 개수
         long boxes = 0L;
         
-        //현재까지의 h
         for(int i = 1; i<w; i++){
+            //h/w는 일차함수 기울기
+            //i를 곱하면 y값 구함 -> y값보다 작은 개수만큼 정사각형
             long n = (long)((double)h * i)/w;
             boxes+=n;
         }
