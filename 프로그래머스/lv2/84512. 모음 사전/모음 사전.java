44d470316bc7569d437ee67f6b41bd5088c75532
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-05-26 17:14:47 +0900

    [level 2] Title: 모음 사전, Time: 54.29 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -1,30 +1,24 @@
 import java.util.*;
 
 class Solution {
-    public List<String> list = new ArrayList<>();
-    public String[] alphabets = {"A", "E", "I", "O", "U"};
+    public String[] words = new String[]{"A", "E", "I", "O", "U"};
+    public List<String> dict = new ArrayList<>();
     
     public void dfs(int idx, String s){
-        if(idx==5){
-            return;
+        if(idx > 5) return;
+        
+        if(!dict.contains(s)) dict.add(s);
+        
+        for(int i = 0; i<words.length; i++){
+            dfs(idx+1, s+words[i]);
         }
         
-        for(int i = 0; i<alphabets.length; i++){
-            list.add(s + alphabets[i]);
-            dfs(idx+1, s + alphabets[i]);
-        }
     }
     
     public int solution(String word) {
+        
         dfs(0, "");
         
-        int count = 0;
-        
-        for(String l : list){
-            count++;
-            if(l.equals(word))
-                return count;
-        }
-        return count;
+        return dict.indexOf(word);
     }
 }
