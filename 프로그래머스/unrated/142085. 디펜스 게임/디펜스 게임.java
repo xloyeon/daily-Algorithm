34a1f778173149fce980c296fdec129294a47d96
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-06-18 01:51:06 +0900

    [unrated] Title: 디펜스 게임, Time: 0.69 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
@@ -4,17 +4,26 @@ import java.util.stream.*;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
         //K를 적이 많은 라운드에서 사용하면 가능 ..?
-        //많은 ROUND K개 제외한 다음, 병사를 소모하며 확인
+        //만약 더 뺄 수 없으면 이전에 값 들 중 가장 큰 값을 무적권 사용하고 계산
+        //매번 정렬 ? -> 우선순위 큐 사용해서 큰 값 구하기
         
-        PriorityQueue<Integer> q = new PriorityQueue<>();
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+        int rest = n;   //남은 병사 수
+        
         for (int i = 0; i < enemy.length; i++) {
+            //지금 병사 수가 이전 적들보다도 큰 값일 수 있으므로 먼저 넣고 확인
+            rest -= enemy[i];
             q.add(enemy[i]);
             
-            if (q.size() > k)
-                n -= q.poll();
-            
-            if (n < 0)
-                return i;
+            if(rest<0){
+                //이전 적 수 중 가장 큰 값을 무적권 사용했다고 하기
+                if(k>0 && !q.isEmpty()){
+                    rest += q.poll();
+                    k--;
+                }else{
+                    return i;
+                }
+            }
         }
         
         return enemy.length;
