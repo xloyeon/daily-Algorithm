9461e8069027a2a06899c63e42333fb4bbb8009a
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-06-25 01:48:23 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
@@ -4,9 +4,8 @@ class Solution {
     public int solution(String name) {
         //알파벳을 바꾸는 수는 항상 같음
         //따라서 최소가 되려면 커서 이동 수를 줄여야 함
-        //만약 똑같이 a라면 이동하지 않고 다음 알파벳을 생각합
+        //만약 똑같이 a라면 다음 알파벳을 생각합
         char[] c = name.toCharArray();
-        int[] visited = new int[c.length];
         
         int count = 0;  //알파벳 바꾸는 조작수
         int move = c.length -1;   //바꿀 알파벳 위치로 이동하는 조작수
@@ -25,7 +24,7 @@ class Solution {
             //현재 이동한 위치에서 다시 돌아가 뒤에서부터 이동해 도달하는 횟수
             move = Math.min(move, i * 2 + c.length - idx);
             
-            //처음부터 뒤로 돌아갔다가 다시 정순으로
+            //처음부터 뒤로 돌아갔다가 다시 정방향으로
             move = Math.min(move, (c.length - idx) * 2 + i);
         }
         
