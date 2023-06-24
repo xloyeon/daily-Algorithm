569a2deb0ae97406ba2c837130564a2619366946
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-06-25 00:19:36 +0900

    [unrated] Title: 과제 진행하기, Time: 10.63 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.java
@@ -23,36 +23,29 @@ class Mission implements Comparable<Mission> {
         return this.time;
     }
     
-    public void setTime(int time){
-        this.time = time;
-    }
-    
     public int compareTo(Mission m){
         return this.start - m.getStart();
     }
 }
 
 class Solution {
-    
     public int convertToMinutes(String time){
         String[] times = time.split(":");
         return Integer.valueOf(times[0])*60 + Integer.valueOf(times[1]);
     }
     
     public String[] solution(String[][] plans) {
-        
         String[] result = new String[plans.length]; //결과 배열
         int idx = 0;    //결과 배열 인덱스
         
         PriorityQueue<Mission> pq = new PriorityQueue<>();  //과제 시작 시간 순서대로
-        List<Mission> stop = new ArrayList<>(); //잠시 멈춘 과제
+        Stack<Mission> stack = new Stack<>(); //잠시 멈춘 과제
         
         for(String[] plan : plans){
             int startTime = convertToMinutes(plan[1]);
             Mission mission = new Mission(plan[0], startTime, Integer.valueOf(plan[2]));
             pq.add(mission);
         }
-        
         
         //이전에 시작한 과제
         Mission before = pq.poll();
@@ -61,14 +54,12 @@ class Solution {
             //다음에 시작하는 과제
             Mission current = pq.poll();
             
-            //다음에 시작하는 과제이전에 수행할 수 있는 시간
+            //다음에 시작하는 과제 이전까지의 수행 가능시간
             int gap = current.getStart() - before.getStart();
             
-            
-            //현재 시작 과제를 다음 시작 과제 전에 끝낼 수 없다면
+            //이전 시작 과제를 다음 시작 과제 전에 끝낼 수 없다면
             if(before.getTime()> gap){
-                before.setTime(before.getTime()-gap);   //할 수 있는 만큼 수행하고
-                stop.add(before);   //잠시 멈춤
+                stack.push(new Mission(before.getName(), before.getStart(), before.getTime()-gap));   //잠시 멈춤
             }
             
             //끝낼 수 있다면
@@ -77,15 +68,13 @@ class Solution {
                 result[idx++] = before.getName();   //현재 시작 과제 끝냄
                 
                 //가장 최근에 멈춘 과제부터 하나씩 끝내기
-                while(stop.size()>0 && gap>0){
-                    Mission temp = stop.get(stop.size()-1);
+                while(!stack.isEmpty() && gap>0){
+                    Mission temp = stack.pop();
                     
                     if(temp.getTime()>gap){
-                        temp.setTime(temp.getTime()-gap);
-                        stop.set(stop.size()-1, temp);
+                        stack.push(new Mission(temp.getName(), temp.getStart(), temp.getTime()-gap));
                         break;
                     }else {
-                        stop.remove(stop.size()-1);
                         result[idx++] = temp.getName();
                         gap -= temp.getTime();
                     }
@@ -98,9 +87,8 @@ class Solution {
         result[idx++] = before.getName();
         
         //잠시 멈춘 과제 중에 끝내지 못한 과제 끝내기
-        while(stop.size()>0){
-            result[idx++] = stop.get(stop.size()-1).getName();
-            stop.remove(stop.size()-1);
+        while(!stack.isEmpty()){
+            result[idx++] = stack.pop().getName();
         }
         
         return result;
