5cc692a08b5bd7b3aee931c77284636b4b106c37
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-07-22 13:18:43 +0900

    [level 3] Title: 단어 변환, Time: 0.24 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -3,37 +3,54 @@ import java.util.*;
 class Solution {
     
     public int solution(String begin, String target, String[] words) {
-        boolean[] visited = new boolean[words.length];
-        int ans = dfs(begin, target, words, visited, 1, Integer.MAX_VALUE);
-        return ans == Integer.MAX_VALUE ? 0 : ans;
-    }
-
-    private int dfs(String begin, String target, String[] words, boolean[] visited, int cnt, int ans) {
-        if (begin.equals(target)) {
-            return cnt - 1;
-        }
-
-        for (int i = 0; i < words.length; i++) {
-            if (!visited[i] && onlyOneDifferentChar(begin, words[i])) {
-                visited[i] = true;
-                ans = Math.min(ans, dfs(words[i], target, words, visited, cnt + 1, ans));
-                visited[i] = false;
+        int[] visited = new int[words.length];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(-1);
+        
+        while(!q.isEmpty()){
+            int temp = q.poll();
+            System.out.println(temp);
+            String word = "";
+            
+            if(temp == -1) 
+                word = begin;
+            else
+                word = words[temp];
+            
+            if(word.equals(target)){
+                return visited[temp];
             }
+            
+            for(int i = 0; i<words.length; i++){
+                if(visited[i] == 0){
+                    if(check(words[i], word)){
+                        q.add(i);
+                        
+                        if(temp == -1){
+                            visited[i] = 1;
+                        } else{
+                            visited[i] = visited[temp]+1;
+                        }
+                    }
+                }
+            }
+                
         }
-        return ans;
+        return 0;
     }
 
-    private boolean onlyOneDifferentChar(String a, String b) {
+    private boolean check(String a, String b) {
         if (a.length() != b.length()) {
             return false;
         }
 
-        int differentCnt = 0;
+        int count = 0;
+        
         for (int i = 0; i < a.length(); i++) {
             if (a.charAt(i) != b.charAt(i)) {
-                differentCnt++;
+                count++;
             }
         }
-        return differentCnt == 1;
+        return count == 1;
     }
 }
