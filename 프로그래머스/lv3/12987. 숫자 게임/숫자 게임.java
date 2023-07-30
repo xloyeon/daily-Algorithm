681b25d96ebb1dfbcdcf691ddc33775c21165e49
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-07-31 01:07:31 +0900

    [level 3] Title: 숫자 게임, Time: 85.49 ms, Memory: 69.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        //a를 내림차순 정렬, b를 내림차순 정렬
+        
+        List<Integer> listA = new ArrayList<>();
+        Deque<Integer> q = new ArrayDeque<>();
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for(int i = A.length-1; i>=0; i--) {
+            listA.add(A[i]);
+        }
+    
+        for(int i = B.length-1; i>=0; i--) {
+            q.addLast(B[i]);
+        }
+        
+        int result = 0;
+        
+        for(int i = 0; i<listA.size(); i++) {
+            if(listA.get(i) < q.peek() ){
+                q.pollFirst();
+                result++;
+            }else {
+                q.pollLast();
+            }
+        }
+        
+        return result;
+        
+        
+    }
+}
