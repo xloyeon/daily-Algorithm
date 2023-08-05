b3ead23ab0884a134db0e6b77f233eab3a4584f2
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-08-05 22:10:19 +0900

    [level 3] Title: 기지국 설치, Time: 0.61 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(int n, int[] stations, int w) {
+        int count = 0;
+        int start = 1;
+        
+        //stations는 오름차순 정렬 되어 있음
+        for(int i = 0; i<stations.length; i++) {
+            if(start < stations[i] -w) {    //왼쪽 전파 범위 전이면
+                int end = stations[i] - w;
+                
+                int temp = (end-start)/(w*2+1);
+                if((end-start)%(w*2+1) != 0)
+                    temp ++;
+                count += temp;
+            }
+            start = stations[i] + w + 1;  // 여기서부터 다시 시작
+        }
+        
+        //station의 마지막 전파 범위가 끝이 아니면
+        if(stations[stations.length-1] + w < n){
+            start = stations[stations.length-1] + w + 1;
+            int end = n+1;
+            
+            int temp = (end-start)/(w*2+1);
+            if((end-start)%(w*2+1) != 0)
+                temp++;
+            count+= temp;
+        }
+        return count;
+            
+    }
+}
