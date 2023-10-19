e677025f3dffbfce854a627a6e52de177971e670
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-10-19 15:42:42 +0900

    [Silver V] Title: 집합, Time: 1072 ms, Memory: 313728 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -7,6 +7,8 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         
         int n = Integer.parseInt(br.readLine());
+        
+        //1이면 집합 포함, 0이면 미포함
         int S = 0;
         
         while(n --> 0) {
@@ -16,18 +18,23 @@ public class Main {
             
             if(str.equals("add")){
                 num = Integer.parseInt(st.nextToken());
-                S |= (1<< num-1);
+                S |= (1<< num-1);    //원소에 해당하는 비트 1
+                
             }else if(str.equals("remove")) {
                 num = Integer.parseInt(st.nextToken());
-                S &= ~(1 << num-1);
+                S &= ~(1 << num-1);    //원소에 해당하는 비트 0
+                
             }else if(str.equals("check")) {
                 num = Integer.parseInt(st.nextToken());
                 sb.append((S & (1<<num-1)) !=0 ? "1\n" : "0\n");
+                
             }else if(str.equals("toggle")) {
                 num = Integer.parseInt(st.nextToken());
-                S ^= (1<<num-1);
+                S ^= (1<<num-1);    //있으면 0, 없으면 1
+                
             }else if(str.equals("all")) {
                 S |= (~0);
+                
             }else if(str.equals("empty")) {
                 S &= 0;
             }
