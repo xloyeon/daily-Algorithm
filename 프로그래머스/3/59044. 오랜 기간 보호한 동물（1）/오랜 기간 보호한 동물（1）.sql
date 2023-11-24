68d5bd9c21fb26462c7196e3d0c8d8d45c505f90
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-11-24 21:03:01 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,14 @@
+-- 아직 입양 못 간 동물
+-- 가장 오래 잇었던 동물 3마리
+-- 이름, 보호 시작일
+-- 시작일 오름차순
+
+SELECT NAME, DATETIME
+FROM ANIMAL_INS 
+WHERE ANIMAL_ID NOT IN 
+
+(SELECT ANIMAL_ID
+FROM ANIMAL_OUTS)
+
+ORDER BY DATETIME ASC
+FETCH FIRST 3 ROWS ONLY;
