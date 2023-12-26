20cc1e4f7605ce8ee02d1203681c0af650ccb2da
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-12-26 13:53:30 +0900

    [Silver II] Title: 에디터, Time: 640 ms, Memory: 83372 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<String> left = new Stack<>();    //커서 앞
+        Stack<String> right = new Stack<>();    //커서 뒤
+        
+        String[] sArr = br.readLine().split("");
+        
+        for(String s : sArr){
+            left.push(s);
+        }
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i<N; i++){
+            String s = br.readLine();
+            char c = s.charAt(0);
+            
+            if(c == 'L' && !left.isEmpty()){
+                right.push(left.pop());
+            }else if(c == 'D' && !right.isEmpty()){
+                left.push(right.pop());
+            }else if(c == 'B' && !left.isEmpty()){
+                left.pop();
+            }else if(c == 'P'){
+                left.push(String.valueOf(s.charAt(2)));
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(!left.isEmpty()){
+            right.push(left.pop());
+        }
+        
+        while(!right.isEmpty()){
+            sb.append(right.pop());
+        }
+        
+        System.out.println(sb.toString());
+    }
+}
